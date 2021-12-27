f363c4562b41f14a71638aca9520abbbcc3feb6a
Evgeny Mironov <e.d.mironov@yandex.ru>
2021-12-27 18:43:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 1500; //стоимость билета
+        int bonusMileCost = 20; //стоимость бонусной мили
+        int Mile = priceTicket / bonusMileCost; //количество бонусных миль
+        System.out.println(Mile);
+    }
+}
